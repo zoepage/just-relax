ca87423aadab68713b165ff769a7a7ef7bba96d3
Madeleine Neumann <madeleine@Madeleines-MacBook-Pro.local>
2015-06-27 21:42:36 +0200

    Add Settings Slider

--- www/assets/js/forms.js
+++ www/assets/js/forms.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-  $(".btn1").click(function(){
-    $("p").fadeOut()
-  });
-  $(".btn2").click(function(){
-    $("p").fadeIn();
-  });
-});
+$(function(){
+  $("button.btn").bind('click', function(event){
+      var e = event.target;
+      var id = e.getAttribute("data-next");
+
+      if (id >= 4) {
+        href.location = 'dashboard.html';
+      } else {
+      $("#data-setup" + (id -1)).fadeOut().addClass("hide");
+      $("#data-setup" + id).removeClass("hide").fadeIn();
+      return false;
+      }
+    })
+})
+
