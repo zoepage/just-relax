86722fa01ea090729896480029331d16c5b4be1f
zoepage <o.gasidlo@gmail.com>
2015-06-27 22:57:49 +0200

    start for result

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -3,20 +3,30 @@
 // initialize Hoodie
 var hoodie  = new Hoodie();
 
+// on ready calls all binds
 $(function () {
-
-  var submitSignUp = function submitSignUp(){
-    var usr = $('#email').val();
-    var pwd = $('#pwd').val();
-    
-    hoodie.account.signIn(usr, pwd).done(function(){
-      if(hoodie.account.username) {
-        location.href = 'http://127.0.0.1:6076/dashboard.html'
-      }
-    })
-
-    return false;
-  }
-  
   $('#sign-up').bind('submit', submitSignUp)
+  $('#go-on-vacation').bind('click', result)
 })
+
+
+// sign-in on main page
+var submitSignUp = function submitSignUp(){
+  var usr = $('#email').val();
+  var pwd = $('#pwd').val();
+  
+  hoodie.account.signIn(usr, pwd).done(function(){
+    if(hoodie.account.username) {
+      location.href = 'http://127.0.0.1:6076/dashboard.html'
+    }
+  })
+  return false;
+}
+
+// return results
+var result = function result(event) {
+  var e = event.target;
+  var url = e.getAttribute('data-url');
+
+  $.ajax()
+}
