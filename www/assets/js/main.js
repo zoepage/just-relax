48498360688a450a696891aa5f1cc2c89e752847
zoepage <o.gasidlo@gmail.com>
2015-06-27 20:21:39 +0200

    chore(index): added missing styles, login + typo

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,2 +1,22 @@
+'use strict'
+
 // initialize Hoodie
 var hoodie  = new Hoodie();
+
+$(function () {
+
+  var submitSignUp = function submitSignUp(){
+    var usr = $('#email').val();
+    var pwd = $('#pwd').val();
+    
+    hoodie.account.signIn(usr, pwd).done(function(){
+      if(hoodie.account.username) {
+        location.href = 'http://127.0.0.1:6076/dashboard.html'
+      }
+    })
+
+    return false;
+  }
+  
+  $('#sign-up').bind('submit', submitSignUp)
+})
