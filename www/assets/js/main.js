8c4bbc7f0fab112f92ac780436a436a947fb3c91
Robin Böhm <robin.boehm@workshops.de>
2019-11-13 00:00:41 +0100

    fix(main): activate superhere functions

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -4,7 +4,7 @@
 // var hoodie = new Hoodie();
 
 // on ready calls all binds
-$(function() {
+$(function () {
   // if (hoodie.account.username) {
   //   hoodie.store.find('settings', 'user-settings')
   //     .done(function(settings) {
@@ -19,7 +19,7 @@ $(function() {
   //     })
   // }
 
-  if($('#dashboard')) {
+  if ($('#dashboard')) {
     const settings = {
       name: localStorage.getItem('name'),
       totalVacDays: localStorage.getItem('totalVacDays'),
@@ -27,7 +27,7 @@ $(function() {
     }
 
     animateDays(vacDays(settings), workDays());
-    
+
   }
 })
 
@@ -43,20 +43,20 @@ function animateDays(vacDay, workDay) {
   var label = createLabel(vacDay, workDay)
   $('#vac-days').addClass(label).animate({
     'border-width': vacW
-  }, 1400, function() {
+  }, 1400, function () {
     $('.vac-days.overlay').append(vacDay.left + ' / ' + vacDay.total).fadeIn();
   });
 
   $('#work-days').addClass(label).animate({
     'border-width': workW
-  }, 1400, function() {
+  }, 1400, function () {
     $('.work-days.overlay').append(workDay.left + ' / ' + workDay.total).fadeIn();
   });
 
- }
+}
 
 function factorForAni(total, left) {
-  console.log("factorForAni", total, left, left/total);
+  console.log("factorForAni", total, left, left / total);
   return left / total;
 }
 
@@ -66,7 +66,7 @@ function createLabel(vacDay, workDay) {
 
   var fac = Math.abs(vac - work);
 
-  if(fac < 0.2) {
+  if (fac < 0.2) {
     return 'green';
   } else if (fac < 0.4) {
     return 'orange';
@@ -77,8 +77,8 @@ function createLabel(vacDay, workDay) {
 
 var workDays = function workDays() {
   return {
-    total: 300,// totalWorkDays(dates),
-    left: 42 // leftWorkDays(dates).leftWorkDays
+    total: totalWorkDays(dates),
+    left: leftWorkDays(dates).leftWorkDays
   };
 }
 
@@ -89,7 +89,7 @@ var vacDays = function vacDays(settings) {
 
   return {
     total: t,
-    left:  v
+    left: v
   };
 }
 
